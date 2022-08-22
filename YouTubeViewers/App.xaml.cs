29db61e64f1161f31bbf55b0d2670218d1b6f042
Guilherme Cabral <guilherme.mcabral@gmail.com>
2022-08-22 18:23:31 -0300

    Initial setup of views

--- YouTubeViewers/App.xaml.cs
+++ YouTubeViewers/App.xaml.cs
@@ -13,5 +13,12 @@ namespace YouTubeViewers
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
