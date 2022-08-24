6d52905d425851bf0835c17c96ae1408edb788e3
Guilherme Cabral <guilherme.mcabral@gmail.com>
2022-08-24 18:49:52 -0300

    View models for modal

---
+++ YouTubeViewers/ViewModels/EditYouTubeViewerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeViewers.ViewModels
+{
+    public class EditYouTubeViewerViewModel : ViewModelBase
+    {
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public EditYouTubeViewerViewModel()
+        {
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel();
+        }
+    }
+}
---
+++ YouTubeViewers/ViewModels/YouTubeViewerDetailsFormViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace YouTubeViewers.ViewModels
+{
+    public class YouTubeViewerDetailsFormViewModel : ViewModelBase
+    {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private bool _isSubscribed;
+
+        public bool Is_Subscribed
+        {
+            get { return _isSubscribed; }
+            set
+            {
+                _isSubscribed = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private bool _isMember;
+
+        public bool IsMember
+        {
+            get { return _isMember; }
+            set
+            {
+                _isMember = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public bool CanSubmit => !string.IsNullOrEmpty(Username);
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+    }
+}
