c91c3d8cae8380b34b61ebc60f39574924b667c9
Guilherme Cabral <guilherme.mcabral@gmail.com>
2022-08-26 14:37:02 -0300

    Services

--- YouTubeViewers/App.xaml.cs
+++ YouTubeViewers/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using YouTubeViewers.Domain.Commands;
+using YouTubeViewers.Domain.Queries;
+using YouTubeViewers.EntityFramework;
+using YouTubeViewers.EntityFramework.Commands;
+using YouTubeViewers.EntityFramework.Queries;
 using YouTubeViewers.Stores;
 using YouTubeViewers.ViewModels;
 
@@ -15,14 +21,33 @@ namespace YouTubeViewers
     /// </summary>
     public partial class App : Application
     {
+        private readonly IGetAllYouTubeViewersQuery _getAllYouTubeViewersQuery;
+        private readonly ICreateYouTubeViewerCommand _createYouTubeViewerCommand;
+        private readonly IUpdateYouTubeViewerCommand _updateYouTubeViewerCommand;
+        private readonly IDeleteYouTubeViewerCommand _deleteYouTubeViewerCommand;
+
+        private readonly YouTubeViewersDbContextFactory _youTubeViewersDbContextFactory;
         private readonly ModalNavigationStore _modalNavigationStore;
         private readonly YouTubeViewersStore _youTubeViewersStore;
         private readonly SelectedYouTubeViewerStore _selectedYouTubeViewerStore;
 
         public App()
         {
+            string _connectionString = "Data Source=YouTubeViewers.db";
+
+            _youTubeViewersDbContextFactory = new YouTubeViewersDbContextFactory(
+                new DbContextOptionsBuilder().UseSqlite(_connectionString).Options);
+            
+            _getAllYouTubeViewersQuery = new GetAllYouTubeViewersQuery(_youTubeViewersDbContextFactory);
+            _createYouTubeViewerCommand = new CreateYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _updateYouTubeViewerCommand = new UpdateYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _deleteYouTubeViewerCommand = new DeleteYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+
             _modalNavigationStore = new ModalNavigationStore();
-            _youTubeViewersStore = new YouTubeViewersStore();
+            _youTubeViewersStore = new YouTubeViewersStore(_getAllYouTubeViewersQuery,
+                                                           _createYouTubeViewerCommand,
+                                                           _updateYouTubeViewerCommand,
+                                                           _deleteYouTubeViewerCommand);
             _selectedYouTubeViewerStore = new SelectedYouTubeViewerStore(_youTubeViewersStore);
         }
 
@@ -30,7 +55,7 @@ namespace YouTubeViewers
         {
             YouTubeViewersViewModel youTubeViewersViewModel = new YouTubeViewersViewModel(
                 _youTubeViewersStore,
-                _selectedYouTubeViewerStore, 
+                _selectedYouTubeViewerStore,
                 _modalNavigationStore);
 
             MainWindow = new MainWindow()
--- YouTubeViewers/Stores/YouTubeViewersStore.cs
+++ YouTubeViewers/Stores/YouTubeViewersStore.cs
@@ -3,22 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouTubeViewers.Domain.Commands;
 using YouTubeViewers.Domain.Models;
+using YouTubeViewers.Domain.Queries;
 
 namespace YouTubeViewers.Stores
 {
     public class YouTubeViewersStore
     {
+        private readonly IGetAllYouTubeViewersQuery _getAllYouTubeViewersQuery;
+        private readonly ICreateYouTubeViewerCommand _createYouTubeViewerCommand;
+        private readonly IUpdateYouTubeViewerCommand _updateYouTubeViewerCommand;
+        private readonly IDeleteYouTubeViewerCommand _deleteYouTubeViewerCommand;
+
+        public YouTubeViewersStore(IGetAllYouTubeViewersQuery getAllYouTubeViewersQuery,
+                                   ICreateYouTubeViewerCommand createYouTubeViewerCommand,
+                                   IUpdateYouTubeViewerCommand updateYouTubeViewerCommand,
+                                   IDeleteYouTubeViewerCommand deleteYouTubeViewerCommand)
+        {
+            _getAllYouTubeViewersQuery = getAllYouTubeViewersQuery;
+            _createYouTubeViewerCommand = createYouTubeViewerCommand;
+            _updateYouTubeViewerCommand = updateYouTubeViewerCommand;
+            _deleteYouTubeViewerCommand = deleteYouTubeViewerCommand;
+        }
+
         public event Action<YouTubeViewer> YouTubeViewerAdded;
         public event Action<YouTubeViewer> YouTubeViewerUpdated;
 
         public async Task Add(YouTubeViewer youTubeViewer)
         {
+            await _createYouTubeViewerCommand.Execute(youTubeViewer);
             YouTubeViewerAdded?.Invoke(youTubeViewer);
         }
 
         public async Task Update(YouTubeViewer youTubeViewer)
         {
+            await _updateYouTubeViewerCommand.Execute(youTubeViewer);
             YouTubeViewerUpdated?.Invoke(youTubeViewer);
         }
     }
